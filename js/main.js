9c4abfa7aa109685a5cdd7cca18459130dfe7281
Handle (Xiaosheng) Huang <hhuang@successfactors.com>
2014-05-06 21:07:38 +0800

    ab

--- js/main.js
+++ js/main.js
@@ -3,8 +3,13 @@ function landingLayout() {
 	$('.landing-section').css({
 		height: WH + 'px'
 	});
-  $('.scroll-down').css({left:(WW/2-31)+'px'});
-  TweenLite.to('.scroll-down',2,{bottom:'20px', ease:Elastic.easeOut});
+	$('.scroll-down').css({
+		left: (WW / 2 - 31) + 'px'
+	});
+	TweenLite.to('.scroll-down', 2, {
+		bottom: '20px',
+		ease: Elastic.easeOut
+	});
 }
 
 function doResize() {
@@ -12,14 +17,36 @@ function doResize() {
 	WH = $(window).height();
 	landingLayout();
 }
+function doScrollSpy() {
+	$('#landing,#about-brand,#brand-news').each(function() {
+		var position = $(this).position();
+    var $this = $(this);
+		$(this).scrollspy({
+			min: position.top - WH / 2,
+			max: position.top + $this.height() + 823,
+			onEnter: function(element, position) {
+				console.log('change bubble');
+        var $el = $(element);
+        var id = $el.attr('id');
+        $('#parallax-nav .active').removeClass('active');
+        $('#parallax-nav a[href="#'+id+'"]').parent().addClass('active');
+			}
+		});
+	});
+}
 
 $(function() {
 	$('.jScrollPane').jScrollPane();
 	$(".img-liquid-fill").imgLiquid();
-	$(".img-liquid-fill-top").imgLiquid({verticalAlign:'top'});
+	$(".img-liquid-fill-top").imgLiquid({
+		verticalAlign: 'top'
+	});
 	$('.top-menu a,.scroll-down,.scroll-up,#parallax-nav a,#move-top').smoothScroll();
-	$('.center').center({against:'parent'});
+	$('.center').center({
+		against: 'parent'
+	});
 	doResize();
+	doScrollSpy();
 	$(window).resize(doResize);
 });
 
